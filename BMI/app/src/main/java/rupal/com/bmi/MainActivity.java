43b56244ebf1b26e21a41aeb42d8a356b5aaf7e4
rmartin <rupal.martin03@gmail.com>
2019-04-30 23:01:12 -0700

    first commit

---
+++ BMI/app/src/main/java/rupal/com/bmi/MainActivity.java
@@ -0,0 +1,108 @@
+package rupal.com.bmi;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private EditText height;
+    private EditText weight;
+    private TextView result;
+    SensorManager sensorManager;
+    TextView tx_steps;
+    boolean running = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.health);
+        height = (EditText) findViewById(R.id.height);
+        weight = (EditText) findViewById(R.id.weight);
+        result = (TextView) findViewById(R.id.result);
+        tx_steps = (TextView) findViewById(R.id.steps);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    public void calculateBMI(View v) {
+        String heightStr = height.getText().toString();
+        String weightStr = weight.getText().toString();
+
+        if (heightStr != null && !"".equals(heightStr)
+                && weightStr != null && !"".equals(weightStr)) {
+            float heightValue = Float.parseFloat(heightStr) / 100;
+            float weightValue = Float.parseFloat(weightStr);
+            float bmi = weightValue / (heightValue * heightValue);
+            displayBMI(bmi);
+        }
+    }
+
+    private void displayBMI(float bmi) {
+        String bmiLabel = "";
+
+        if (Float.compare(bmi, 15f) <= 0) {
+            bmiLabel = getString(R.string.very_severely_underweight);
+        } else if (Float.compare(bmi, 15f) > 0 && Float.compare(bmi, 16f) <= 0) {
+            bmiLabel = getString(R.string.severely_underweight);
+        } else if (Float.compare(bmi, 16f) > 0 && Float.compare(bmi, 18.5f) <= 0) {
+            bmiLabel = getString(R.string.underweight);
+        } else if (Float.compare(bmi, 18.5f) > 0 && Float.compare(bmi, 25f) <= 0) {
+            bmiLabel = getString(R.string.normal);
+        } else if (Float.compare(bmi, 25f) > 0 && Float.compare(bmi, 30f) <= 0) {
+            bmiLabel = getString(R.string.overweight);
+        } else if (Float.compare(bmi, 30f) > 0 && Float.compare(bmi, 35f) <= 0) {
+            bmiLabel = getString(R.string.obese_class_i);
+        } else if (Float.compare(bmi, 35f) > 0 && Float.compare(bmi, 40f) <= 0) {
+            bmiLabel = getString(R.string.obese_class_ii);
+        } else {
+            bmiLabel = getString(R.string.obese_class_iii);
+        }
+        bmiLabel = bmi + "\n\n" + bmiLabel;
+        result.setText(bmiLabel);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        running = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (countSensor != null) {
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Toast.makeText(this, "Sensor not found", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        running = false;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (running) {
+            if (event.values[0] > 30000) {
+                //Toast.makeText(this, "Your pet has already taken healthy number of steps ", Toast.LENGTH_SHORT).show();
+            } else {
+                //Toast.makeText(this, "Your pet needs a walk", Toast.LENGTH_SHORT).show();
+            }
+            tx_steps.setText(String.valueOf(event.values[0]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+}
